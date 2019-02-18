a477d291679b4938de9d633b9ae32821357b0b0d
Hossein Khanmirzaei <hossein.khanmirzaei@gmail.com>
2019-02-18 22:24:27 +0330

    (97-11-29 10:30 PM)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { IdentityPageComponent } from './identity-page/identity-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Initial', component: InitialPageComponent },
+  { path: 'Identity', component: IdentityPageComponent },
+  { path: '', component: InitialPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { IdentityPageComponent } from './identity-page/identity-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialPageComponent,
+    IdentityPageComponent
   ],
   imports: [
     BrowserModule,
