79c5b52a01d04abc76fd6077022919064df9be45
Hossein Khanmirzaei <hossein.khanmirzaei@gmail.com>
2019-02-19 22:32:44 +0330

    (97-11-30 10:30 PM)

--- src/app/identity-page/identity-page.component.ts
+++ src/app/identity-page/identity-page.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-identity-page',
   templateUrl: './identity-page.component.html',
-  styleUrls: ['./identity-page.component.css']
+  styleUrls: ['./identity-page.component.css'],
+  host: {
+    class: 'h-100 w-100'
+  }
 })
 export class IdentityPageComponent implements OnInit {
 
--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-initial-page',
   templateUrl: './initial-page.component.html',
-  styleUrls: ['./initial-page.component.css']
+  styleUrls: ['./initial-page.component.css'],
+  host: {
+    class: 'h-100 w-100'
+  }
 })
 export class InitialPageComponent implements OnInit {
 
